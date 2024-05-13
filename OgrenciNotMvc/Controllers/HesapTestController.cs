6972735234fbcb3ba18376f1deca9bc3efbeaff1
Avnullah Ates <avnullahates@gmail.com>
2024-05-13 14:04:06 +0300

    first initial

---
+++ OgrenciNotMvc/Controllers/HesapTestController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OgrenciNotMvc.Controllers
+{
+    public class HesapTestController : Controller
+    {
+        // GET: HesapTest
+        public ActionResult Index(double sayi1 = 0, double sayi2 = 0)
+        {
+
+            double sonucTopla = sayi1 + sayi2;
+            ViewBag.sncTopla = sonucTopla;
+
+            double sonucCikarma = sayi1 - sayi2;
+            ViewBag.sncCikarma = sonucCikarma;
+
+            double sonucCarpma = sayi1 * sayi2;
+            ViewBag.sncCarpma = sonucCarpma;
+
+            if (sayi2 == 0)
+            {
+                Console.WriteLine("sayi2 0 olamaz");
+
+            }
+            else
+            {
+                double sonucBolme = sayi1 / sayi2;
+                ViewBag.scnBolme = sonucBolme;
+            }
+
+
+
+
+            return View();
+        }
+    }
+}
---
+++ OgrenciNotMvc/Controllers/OgrenciController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OgrenciNotMvc.Models.EntityFramwork;
+
+namespace OgrenciNotMvc.Controllers
+{
+    public class OgrenciController : Controller
+    {
+        // GET: Ogrenci
+        DbMvcOkulEntities db = new DbMvcOkulEntities();
+        public ActionResult Index()
+        {
+            var ogrenciler = db.TBLOGRENCILER.ToList();
+            return View(ogrenciler);
+        }
+
+        [HttpGet]
+        public ActionResult YeniOgrenci()
+        {
+
+            List<SelectListItem> degerler = (from i in db.TBLKULUPLER.ToList()
+                                             select new SelectListItem
+                                             {
+                                                 Text = i.KULUPAD,
+                                                 Value = i.KULUPID.ToString()
+                                             }).ToList();
+
+            ViewBag.dgr = degerler;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult YeniOgrenci(TBLOGRENCILER p3)
+        {
+            var klp = db.TBLKULUPLER.Where(x => x.KULUPID == p3.TBLKULUPLER.KULUPID).FirstOrDefault();
+            p3.TBLKULUPLER = klp;
+            db.TBLOGRENCILER.Add(p3);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
+        public  ActionResult Sil (int id)
+        {
+            var ogreci = db.TBLOGRENCILER.Find(id);
+            db.TBLOGRENCILER.Remove(ogreci);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult OgrenciGetir(int id)
+        {
+            var ogrenci = db.TBLOGRENCILER.Find(id);
+            return View("OgrenciGetir", ogrenci);
+
+        }
+        public ActionResult Guncelle(TBLOGRENCILER p)
+        {
+            var ogr = db.TBLOGRENCILER.Find(p.OGRENCIID);
+            ogr.OGRAD = p.OGRAD;
+            ogr.OGRSOYADI = p.OGRSOYADI;
+            ogr.OGRFOTOGRAF = p.OGRFOTOGRAF;
+            ogr.OGRCINSIYET = p.OGRCINSIYET;
+            ogr.OGRKULUP = p.OGRKULUP;
+            db.SaveChanges();
+            return RedirectToAction("Index", "Ogrenci");
+        }
+    }
+}
